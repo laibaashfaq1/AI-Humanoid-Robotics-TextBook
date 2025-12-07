98ff595ed367825d0e8c67a8b32cc2c706c673e6
@LaibaAshfaq2 <165155950+laibaashfaq1@users.noreply.github.com>
2025-12-07 11:06:38 +0500

    solving error

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   url: 'https://laibaashfaq1.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/book-hackathon/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -62,7 +62,7 @@ const config: Config = {
       title: 'AI & Humanoid Robotics Book',
       logo: {
         alt: 'Book Icon',
-        src: 'img/book-icon.svg',
+        src: '/img/book-icon.svg', //logo
       },
       items: [
         {
